ea31b607fc862f61163b114934a3b3274684805f
Sunil Poudel <suneelpdl90@gmail.com>
2025-02-07 06:53:30 +0545

    make home screen (design only)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.sunil_poudel.smemory"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.sunil_poudel.smemory"
