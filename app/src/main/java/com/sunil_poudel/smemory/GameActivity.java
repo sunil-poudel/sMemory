40ba9bad66ab8e83ff8007d6cd1eaf0e5b68e4b3
Sunil Poudel <suneelpdl90@gmail.com>
2025-02-07 13:10:17 +0545

    add onClick listener to buttons on home page

---
+++ app/src/main/java/com/sunil_poudel/smemory/GameActivity.java
@@ -0,0 +1,19 @@
+package com.sunil_poudel.smemory;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class GameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+    }
+}
--- app/src/main/java/com/sunil_poudel/smemory/MainActivity.java
+++ app/src/main/java/com/sunil_poudel/smemory/MainActivity.java
@@ -1,6 +1,9 @@
 package com.sunil_poudel.smemory;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,12 +11,38 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button twoPlayerButton;
+    private Button threePlayerButton;
+    private Button fourPlayerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        twoPlayerButton = findViewById(R.id.two_players_button);
+        threePlayerButton = findViewById(R.id.three_players_button);
+        fourPlayerButton = findViewById(R.id.four_players_button);
+
+        twoPlayerButton.setOnClickListener(this);
+        threePlayerButton.setOnClickListener(this);
+        fourPlayerButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(MainActivity.this, GameActivity.class);
+
+        if (v.getId() == R.id.two_players_button) {
+            intent.putExtra("player_count", "2");
+        } else if(v.getId() == R.id.three_players_button){
+            intent.putExtra("player_count", "3");
+        } else if (v.getId() == R.id.four_players_button) {
+            intent.putExtra("player_count", "4");
+        }
+
+        startActivity(intent);
     }
 }
